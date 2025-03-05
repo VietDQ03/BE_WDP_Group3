c94439556b8b7fd05c2364cd7ff820d0dc7e5e1f
Đặng Quốc Việt <dangquocviet@192.168.2.30>
2025-03-05 17:18:04 +0700

    fix update user

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,12 +1,30 @@
 import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsMongoId, IsNotEmpty } from 'class-validator';
+import {
+  IsMongoId,
+  IsNotEmpty,
+  IsObject,
+  IsOptional,
+  ValidateNested,
+} from 'class-validator';
 import mongoose from 'mongoose';
+import { Type } from 'class-transformer';
+class Company {
+  @IsOptional()
+  _id: mongoose.Schema.Types.ObjectId;
 
+  @IsOptional()
+  name: string;
+}
 export class UpdateUserDto extends PartialType(
   OmitType(CreateUserDto, ['password', 'email'] as const),
 ) {
-  @IsNotEmpty({ message: 'Role không được để trống' })
+  @IsOptional({ message: 'Role không được để trống' })
   @IsMongoId({ message: 'Role có định dạng là mongo id' })
   role: mongoose.Schema.Types.ObjectId;
+  @IsOptional()
+  @ValidateNested()
+  @IsObject()
+  @Type(() => Company)
+  company: Company;
 }
